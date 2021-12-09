93f9bd14a63cd1d63ec56405f631bf88c40c13dd
DiegoBarney <komyhu@hotmail.com>
2021-12-09 13:02:34 -0300

    InitialCommit

---
+++ TesteDllExport/Class1.cs
@@ -0,0 +1,15 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Runtime.InteropServices;
+
+namespace TesteDllExport
+{
+    public class Class1
+    {
+        [DllExport("SomaValor", CallingConvention = CallingConvention.StdCall)]
+        public static int SomaValor(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
