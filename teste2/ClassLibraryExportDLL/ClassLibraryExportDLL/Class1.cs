7c3803c182a6af60c4345efa9b13e030ccb5d506
DiegoBarney <komyhu@hotmail.com>
2021-12-09 13:06:53 -0300

    project2

---
+++ teste2/ClassLibraryExportDLL/ClassLibraryExportDLL/Class1.cs
@@ -0,0 +1,21 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace ClassLibraryExportDLL
+{
+    public class Class1
+    {
+        [DllExport("SomaValor", CallingConvention = CallingConvention.StdCall)]
+        public static int SomaValor(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
